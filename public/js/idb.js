001e4b6de12ce2a03c6e094b92261751a56d308a
esroleo <esroleo@gmail.com>
2021-04-15 14:18:21 -0400

    pwa testing

---
+++ public/js/idb.js
@@ -0,0 +1,58 @@
+// The object stored will come from the catch function
+// fetch() --> .catch() in index.js
+
+// create variable to hold db connection
+let db;
+// establish a connection to IndexedDB database 
+// called 'new_transaction' and set it to version 1
+// new_transaction is the name of the new database dreated by indexedDB
+// or connect to if exist.
+const request = indexedDB.open('budget_tracker', 1);
+
+// this event will emit if the database version changes
+// (nonexistant to version 1, v1 to v2, etc.)
+request.onupgradeneeded = function(event) {
+    // save a reference to the database 
+    const db = event.target.result;
+    // create an object store (table) called `new_transaction`, set it to have an auto incrementing primary key of sorts 
+    db.createObjectStore('new_transaction', { autoIncrement: true });
+  };
+
+  // upon a successful 
+request.onsuccess = function(event) {
+    // when db is successfully created with its object store
+    //  (from onupgradedneeded event above) or simply established
+    //   a connection, save reference to db in global variable
+    db = event.target.result;
+  
+    // check if app is online, if yes run uploadTransaction() function 
+    //to send all local db data to api
+    if (navigator.onLine) {
+        //if online upload all data in the data store to our website
+        // that is if our app is online
+        uploadTransaction();
+    }
+  };
+  
+  request.onerror = function(event) {
+    // log error here
+    console.log(event.target.errorCode);
+  };
+
+
+  // This function will be executed if we attempt to 
+  //submit a new transaction and there's no internet connection
+function saveRecord(record) {
+    // open a new transaction with the database with 
+    // read and write permissions 
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+  
+    // access the object store for `new_transaction`
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+  
+    // add record to your store with add method
+    budgetObjectStore.add(record);
+  }
+
+// listen for app coming back online
+window.addEventListener('online', uploadTransaction);
---
+++ public/js/index.js
@@ -0,0 +1,209 @@
+let transactions = [];
+let myChart;
+
+fetch("/api/transaction")
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    // save db data on global variable
+    transactions = data;
+
+    populateTotal();
+    populateTable();
+    populateChart();
+  });
+
+function populateTotal() {
+  // reduce transaction amounts to a single total value
+  let total = transactions.reduce((total, t) => {
+    return total + parseInt(t.value);
+  }, 0);
+
+  let totalEl = document.querySelector("#total");
+  totalEl.textContent = total;
+}
+
+function populateTable() {
+  let tbody = document.querySelector("#tbody");
+  tbody.innerHTML = "";
+
+  transactions.forEach(transaction => {
+    // create and populate a table row
+    let tr = document.createElement("tr");
+    tr.innerHTML = `
+      <td>${transaction.name}</td>
+      <td>${transaction.value}</td>
+    `;
+
+    tbody.appendChild(tr);
+  });
+}
+
+function populateChart() {
+  // copy array and reverse it
+  let reversed = transactions.slice().reverse();
+  let sum = 0;
+
+  // create date labels for chart
+  let labels = reversed.map(t => {
+    let date = new Date(t.date);
+    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+  });
+
+  // create incremental values for chart
+  let data = reversed.map(t => {
+    sum += parseInt(t.value);
+    return sum;
+  });
+
+  // remove old chart if it exists
+  if (myChart) {
+    myChart.destroy();
+  }
+
+  let ctx = document.getElementById("myChart").getContext("2d");
+
+  myChart = new Chart(ctx, {
+    type: 'line',
+      data: {
+        labels,
+        datasets: [{
+            label: "Total Over Time",
+            fill: true,
+            backgroundColor: "#6666ff",
+            data
+        }]
+    }
+  });
+}
+
+function sendTransaction(isAdding) {
+  let nameEl = document.querySelector("#t-name");
+  let amountEl = document.querySelector("#t-amount");
+  let errorEl = document.querySelector(".form .error");
+
+  // validate form
+  if (nameEl.value === "" || amountEl.value === "") {
+    errorEl.textContent = "Missing Information";
+    return;
+  }
+  else {
+    errorEl.textContent = "";
+  }
+
+  // create record
+  let transaction = {
+    name: nameEl.value,
+    value: amountEl.value,
+    date: new Date().toISOString()
+  };
+
+  // if subtracting funds, convert amount to negative number
+  if (!isAdding) {
+    transaction.value *= -1;
+  }
+
+  // add to beginning of current array of data
+  transactions.unshift(transaction);
+
+  // re-run logic to populate ui with new record
+  populateChart();
+  populateTable();
+  populateTotal();
+  
+  // also send to server
+  fetch("/api/transaction", {
+    method: "POST",
+    body: JSON.stringify(transaction),
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-Type": "application/json"
+    }
+  })
+  .then(response => {    
+    return response.json();
+  })
+  .then(data => {
+    if (data.errors) {
+      errorEl.textContent = "Missing Information";
+    }
+    else {
+      // clear form
+      nameEl.value = "";
+      amountEl.value = "";
+    }
+  })
+  .catch(err => {
+    console.log(err)
+    // fetch failed, so save in indexed db
+    saveRecord(transaction);
+
+    // clear form
+    nameEl.value = "";
+    amountEl.value = "";
+  });
+}
+
+
+// indexDB upload transaction
+
+function uploadTransaction() {
+  // open a transaction on your db
+  const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+  // access your object store
+  const budgetObjectStore = transaction.objectStore('new_transaction');
+
+  // get all records from store and set to a variable
+  // getAll() is an asynchronous function that we have to attach
+  // an event handler in order to retrieve the data
+  // once getAll() completed, it will have a .result property
+  // that's an array variable with all the data from new_transaction object store
+  const getAll = budgetObjectStore.getAll();
+
+
+  // upon a successful .getAll() execution, run this function
+  // Moongoose .create() can hangle a sinble object or an array of objects
+  // there is no need for another route
+  getAll.onsuccess = function() {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(['new_transaction'], 'readwrite');
+          // access the new_transaction object store
+          const budgetObjectStore = transaction.objectStore('new_transaction');
+          // clear all items in your store
+          budgetObjectStore.clear();
+
+          alert('All saved transactions have been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
+}
+
+
+
+document.querySelector("#add-btn").onclick = function() {
+  sendTransaction(true);
+};
+
+document.querySelector("#sub-btn").onclick = function() {
+  sendTransaction(false);
+};
