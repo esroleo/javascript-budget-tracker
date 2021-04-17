3c65079ba194d61edf9f7cb685072970e83609f6
esroleo <esroleo@gmail.com>
2021-04-17 08:24:47 -0400

    testing

--- public/service-worker.js
+++ public/service-worker.js
@@ -63,9 +63,35 @@ self.addEventListener('activate', function(e) {
   });
 
 
-// we need to retrieve information frm the cache
+// we need to retrieve information frm the cache 
 self.addEventListener('fetch', function (e) {
     console.log('fetch request : ' + e.request.url)
+
+    if (evt.request.url.includes('/api/transaction')) {
+      evt.respondWith(
+        caches
+          .open(DATA_CACHE_NAME)
+          .then(cache => {
+            return fetch(evt.request)
+              .then(response => {
+                // If the response was good, clone it and store it in the cache.
+                if (response.status === 200) {
+                  cache.put(evt.request.url, response.clone());
+                }
+  
+                return response;
+              })
+              .catch(err => {
+                // Network request failed, try to get it from the cache.
+                return cache.match(evt.request);
+              });
+          })
+          .catch(err => console.log(err))
+      );
+  
+      return;
+    }
+
     e.respondWith(
         caches.match(e.request).then(function (request) {
             if (request) {
